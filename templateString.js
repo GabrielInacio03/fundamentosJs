64cbd9b66c7f9395a54f284b27e354aae694a181
inacio <inaciogs03@gmail.com>
2021-11-24 09:16:50 -0300

    Template String

---
+++ templateString.js
@@ -0,0 +1,16 @@
+const nome = "Rebeca";
+const concatenacao =  "Olá" +
+ nome +
+  "!"
+
+//template string
+const template = `Olá ${nome}`
+
+console.log(template)
+//${nome} => interpolação( introdução de palavras ou frases em um texto)
+
+//Expressoes...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase() //funcao estranha
+console.log(`Ei... ${up('cuidado')}`)

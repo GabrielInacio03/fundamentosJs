4b85a6c9f994cbe0edad07b4f05bdc9bac649a76
Gabriel Inacio <inaciogs03@gmail.com>
2021-12-27 15:53:06 -0300

    arrays em javascript

---
+++ array.js
@@ -0,0 +1,12 @@
+const valores = [7.7,8.9,10,2.2]
+//console.log(valores)
+
+console.log(valores[0])
+console.log(valores.length)
+
+valores.push(20)
+
+console.log(valores)
+
+valores.pop() //retira o ultimo valor de um array
+delete valores[0] //deleta por indice

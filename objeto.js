eeee63e5732eb2c9ae9d1bf435edd32e4bb318dd
Gabriel Inacio <inaciogs03@gmail.com>
2021-12-28 10:19:50 -0300

    objeto

---
+++ objeto.js
@@ -0,0 +1,15 @@
+//Objeto é uma coleção de chave e valor
+const prod1 = {}
+prod1.nome = 'NOTEBOOK'
+prod1.Preco = 1499.99
+prod1.Marca = 'COMPAQ'
+prod1['Desconto'] = 100
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Preta',
+    preco: 79.90
+}
+
+console.log(prod2)
